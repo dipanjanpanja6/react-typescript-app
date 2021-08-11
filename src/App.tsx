a89637b4c5d65e1abd7f12815d66c70506a85305
Dipanjan Panja <dipanjanpanja6@gmail.com>
2021-08-11 19:18:51 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
 import React, { useState } from "react"
 import "./App.css"
 
+interface test {
+  [a: string]: string
+}
+
 function App() {
-  const [state, setState] = useState({ inputValue: "", option: [{ timestamp: "", value: "" }] })
+  const inputValue: test = { a: "", b: "", c: "" }
+
+  const [state, setState] = useState({ inputValue, option: [{ timestamp: "", value: inputValue }] })
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setState({ ...state, inputValue: e.target.value })
+    setState({ ...state, inputValue: { ...state.inputValue, [e.target.id]: e.target.value } })
   }
   const handleClick = () => {
     setState({ ...state, option: [...state.option, { timestamp: new Date().toString(), value: state.inputValue }] })
   }
   const handleOption = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setState({ ...state, inputValue: e.target.value })
+    setState({ ...state, inputValue: state.option.find(d => d.timestamp === e.target.value)?.value ?? inputValue })
   }
   return (
     <div className="App">
-      <input value={state.inputValue} onChange={handleChange} />
+      <input value={state.inputValue.a} id="a" onChange={handleChange} />
+      <input value={state.inputValue.b} id="b" onChange={handleChange} />
+      <input value={state.inputValue.c} id="c" onChange={handleChange} />
+
       <button onClick={handleClick}>save</button>
       <br />
       <select onChange={handleOption}>
         {state.option.map(d => (
-          <option value={d.value}>{d.timestamp}</option>
+          <option value={d.timestamp}>{d.timestamp}</option>
         ))}
       </select>
     </div>
