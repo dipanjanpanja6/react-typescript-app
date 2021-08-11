91a97361629b971cc2ea9a332968f72fb118634e
Dipanjan Panja <dipanjanpanja6@gmail.com>
2021-08-11 17:56:26 +0530

    task done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
 
 function App() {
+  const [state, setState] = useState({ inputValue: "", option: [""] })
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setState({ ...state, inputValue: e.target.value })
+  }
+  const handleClick = () => {
+    setState({ ...state, option: [...state.option, new Date().toString()] })
+  }
+  const handleOption = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setState({ ...state, inputValue: e.target.value })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={state.inputValue} onChange={handleChange} />
+      <button onClick={handleClick}>save</button>
+      <br />
+      <select onChange={handleOption}>
+        {state.option.map(d => (
+          <option value={d}>{d}</option>
+        ))}
+      </select>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
